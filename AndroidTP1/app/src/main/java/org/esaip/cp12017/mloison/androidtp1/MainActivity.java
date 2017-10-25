f19550bd82d75a2a13e0475ace25421e7fbd0779
mloison <merlin.loison@orange.fr>
2017-10-25 10:26:05 +0200

    ajout progress bar

--- AndroidTP1/app/src/main/java/org/esaip/cp12017/mloison/androidtp1/MainActivity.java
+++ AndroidTP1/app/src/main/java/org/esaip/cp12017/mloison/androidtp1/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.oc.hnapp.android.HNQueryTask;
 
@@ -15,15 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //creation du RecyclerView
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        //creation de l'HNArticle Adapter
         HNArticlesAdapter adapter = new HNArticlesAdapter();
         recyclerView.setAdapter(adapter);
 
-        _task = new HNQueryTask(adapter, 80, 1);
+        _task = new HNQueryTask(adapter, 200, 1);
         _task.execute();
+
+        final ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar);
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                progress.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
